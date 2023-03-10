5d3f6ed87d74a6cca61a790624c6ccc05f4355c2
SingaraSDS <singara@sdsharp.com>
2023-03-09 23:02:29 -0500

    logs mongoose

--- server/db.js
+++ server/db.js
@@ -2,16 +2,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config()
-var options = {
-    logger: console.log,
-    loggerLevel: 'info'
-} 
 export const connectDB = async() => {
     try {
         mongoose.set('strictQuery', false)
         mongoose.set('debug', true)
         console.log(`Mongo Connected:debe entrar`);
-        const conn = await mongoose.connect('mongodb://127.0.0.1:27017/merndb')
+        const conn = await mongoose.connect(process.env.URL_BD)
         console.log(`Mongo Connected: ${conn.connection.name}`);
     } catch (error) {
         console.error(`error: ${error.message}`)
